91848c26403d96d3fb1ba071f9ba49e254048d35
Mohamed Shawky Bayoumi <mohamedshawkybayoumi@gmail.com>
2018-07-13 18:15:29 +0200

    make a move function to move the books

--- src/BookChanger.js
+++ src/BookChanger.js
@@ -2,7 +2,8 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 
 class BookChanger extends React.Component {
-
+    state = { books: [] }
+    
     changeShelf = (newBook,newShelf) => {
         BooksAPI.update(newBook,newShelf).then(response => {
             console.log(response)
@@ -12,8 +13,9 @@ class BookChanger extends React.Component {
       
 
     render(){
+        const { book } = this.props
         return (
-            <select onChange={(event) => this.changeShelf(event.target.value)}>
+            <select onChange={(event) => this.changeShelf(book, event.target.value)}>
                 <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
