875ebdb6a0fb11b7209f56cd5ca06ddce6080663
Mohamed Shawky Bayoumi <mohamedshawkybayoumi@gmail.com>
2019-07-27 01:26:22 +0200

    Updates

---
+++ precache-manifest.16c86bed55b3b512de9670da0db7f09d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/MyReads-A-Book-Tracking-App/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/MyReads-A-Book-Tracking-App/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/MyReads-A-Book-Tracking-App/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "e052a7482cf137ff3a02",
+    "url": "/MyReads-A-Book-Tracking-App/static/js/runtime~main.e052a748.js"
+  },
+  {
+    "revision": "3ac57558f995a16f34f5",
+    "url": "/MyReads-A-Book-Tracking-App/static/js/main.3ac57558.chunk.js"
+  },
+  {
+    "revision": "6d62c37c0f0412ef0334",
+    "url": "/MyReads-A-Book-Tracking-App/static/js/2.6d62c37c.chunk.js"
+  },
+  {
+    "revision": "3ac57558f995a16f34f5",
+    "url": "/MyReads-A-Book-Tracking-App/static/css/main.ae2f5b20.chunk.css"
+  },
+  {
+    "revision": "48f31a4d61acd38268d98ef55285f1d9",
+    "url": "/MyReads-A-Book-Tracking-App/index.html"
+  }
+];
